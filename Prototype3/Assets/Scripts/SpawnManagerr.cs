de614e9972a6927cf9196779e1ebf3b78ac2e3c2
Sable405 <krismontes777@gmail.com>
2023-10-12 11:39:59 -0700

    3.2

--- Prototype3/Assets/Scripts/SpawnManagerr.cs
+++ Prototype3/Assets/Scripts/SpawnManagerr.cs
@@ -8,10 +8,13 @@ public class SpawnManagerr : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = 
+            GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -21,6 +24,10 @@ public class SpawnManagerr : MonoBehaviour
     }
     void SpawnObstacle()
     {  
+        if (playerControllerScript.gameOver == false)
+        {
         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+        
     }
 }
